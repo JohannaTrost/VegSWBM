f4fef928b071760018ea3faa0e323b2990dd84eb
Motzemoere <motze.more@gmx.net>
2024-01-19 11:55:09 +0100

    Update main file

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# %%
+# os.chdir('..')
+# %%
+# Imports
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.optimize import minimize
+
+from src.swbm import *
+from src.plots import *
+from src.utils import *
+
+#%%
+# Load and pre-process data
+input_swbm_raw = pd.read_csv('data/Data_swbm_Germany.csv')
+input_swbm = prepro(input_swbm_raw)
