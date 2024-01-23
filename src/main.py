26c8508f359e23eb32554428820bfc074e95a5fd
johanna.trost <johanna.trost@univ-lyon1.fr>
2024-01-23 17:44:56 +0100

    Fixed main script.

--- src/main.py
+++ src/main.py
@@ -2,26 +2,6 @@
 # os.chdir('..')
 # %%
 # Imports
-import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy.optimize import minimize
-
-from src.swbm import *
-from src.plots import *
-from src.utils import *
-
-#%%
-# Load and pre-process data
-input_swbm_raw = pd.read_csv('data/Data_swbm_Germany.csv')
-input_swbm = prepro(input_swbm_raw)
-
-#%%
-# Calibration (opt c_s, g, a, b0 seasonal)
-
-# %%
-# os.chdir('..')
-# %%
-# Imports
 from scipy.optimize import minimize
 
 from src.plots import *
